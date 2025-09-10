0dfa2c0d58fcce4e78c4b7f4ae17ff6b9a140cf3
Voguh <voguhofc@protonmail.com>
2025-09-10 14:29:46 -0300

    feat: vanilla parts for 69mini

---
+++ Contents/mods/DAMNVanillaPartsAddonBase/media/lua/client/DVPA_69mini.lua
@@ -0,0 +1,59 @@
+--[[
+/*!******************************************************************************
+ * DAMN Vanilla Parts Addon
+ * Copyright (C) 2025 Voguh <voguhofc@protonmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+ ******************************************************************************/
+]]
+
+if not DAMNVanillaPartsAddon then
+    require("DVPA_ClientMain");
+end
+
+local parts = DAMNVanillaPartsAddon.parts;
+local function swapPartsForVehicle(vehicleId)
+    -- Tires
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "TireFrontLeft", parts.standardTires);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "TireFrontRight", parts.standardTires);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "TireRearLeft", parts.standardTires);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "TireRearRight", parts.standardTires);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "MINI69SpareTire", parts.standardTires);
+
+    -- Seats
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "SeatFrontLeft", parts.standardSeats);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "SeatFrontRight", parts.standardSeats);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "SeatRearLeft", parts.standardSeats);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "SeatRearRight", parts.standardSeats);
+
+    -- Bodywork
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "EngineDoor", parts.standardHood);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "TrunkDoor", parts.standardTrunkLid);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "Windshield", parts.standardWindshield);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "WindshieldRear", parts.standardRearWindshield);
+
+    -- Doors
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "WindowFrontLeft", parts.standardFrontWindows);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "WindowFrontRight", parts.standardFrontWindows);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "WindowRearLeft", parts.standardRearWindows);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "WindowRearRight", parts.standardRearWindows);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "DoorFrontLeft", parts.standardFrontDoors);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "DoorFrontRight", parts.standardFrontDoors);
+end
+
+local function onLoad()
+    if DAMNVanillaPartsAddon:enableFor("69mini") then
+        swapPartsForVehicle("Base.69mini");
+        swapPartsForVehicle("Base.69miniIJ");
+        swapPartsForVehicle("Base.69miniMrB");
+        swapPartsForVehicle("Base.69miniPS");
+        swapPartsForVehicle("Base.69miniUnionJack");
+    end
+end
+
+Events.OnLoad.Add(onLoad);
