2c52bb275e8ebb1cb7587576ae1dd39d9ee3a9b2
Voguh <voguhofc@protonmail.com>
2025-09-10 13:16:12 -0300

    feat: vanilla parts for 67gt500

---
+++ Contents/mods/DAMNVanillaPartsAddonBase/media/lua/client/DVPA_67gt500.lua
@@ -0,0 +1,54 @@
+--[[
+/*!******************************************************************************
+ * DAMN Vanilla Parts Addon
+ * Copyright (C) 2025 Voguh <voguhofc@protonmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+ ******************************************************************************/
+]]
+
+if not DAMNVanillaPartsAddon then
+    require("DVPA_ClientMain");
+end
+
+local parts = DAMNVanillaPartsAddon.parts;
+local function swapPartsForVehicle(vehicleId)
+    -- Tires
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "TireFrontLeft", parts.sportTires);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "TireFrontRight", parts.sportTires);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "TireRearLeft", parts.sportTires);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "TireRearRight", parts.sportTires);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "GT500SpareTire", parts.sportTires);
+
+    -- Seats
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "SeatFrontLeft", parts.sportSeats);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "SeatFrontRight", parts.sportSeats);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "SeatRearLeft", parts.sportSeats);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "SeatRearRight", parts.sportSeats);
+
+    -- Bodywork
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "EngineDoor", parts.sportHood);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "TrunkDoor", parts.sportTrunkLid);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "Windshield", parts.sportWindshield);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "WindshieldRear", parts.sportRearWindshield);
+
+    -- Doors
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "DoorFrontLeft", parts.sportFrontDoors);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "DoorFrontRight", parts.sportFrontDoors);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "WindowFrontLeft", parts.sportFrontWindows);
+    DAMNVanillaPartsAddon:replaceVehiclePart(vehicleId, "WindowFrontRight", parts.sportFrontWindows);
+end
+
+local function onLoad()
+    if DAMNVanillaPartsAddon:enableFor("67gt500") then
+        swapPartsForVehicle("Base.67gt500");
+        swapPartsForVehicle("Base.67gt500e");
+    end
+end
+
+Events.OnLoad.Add(onLoad);
